efd72ff0dc006996a52bdf3f24063aabc8eaabcd
Anahita <anahita.tavakoli71@gmail.com>
2024-06-21 23:48:31 +0200

    Update searchScreen ui

--- app/src/main/java/com/neatplex/nightell/component/CustomOutlineTextField.kt
+++ app/src/main/java/com/neatplex/nightell/component/CustomOutlineTextField.kt
@@ -1,10 +1,24 @@
 package com.neatplex.nightell.component
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Icon
+import androidx.compose.material.LocalTextStyle
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.material.TextFieldDefaults
@@ -13,10 +27,23 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawWithCache
+import androidx.compose.ui.graphics.BlendMode
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.LinearGradient
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.neatplex.nightell.R
+import com.neatplex.nightell.ui.theme.myLinearGradiant
 
 @Composable
 fun MyTitleOutlinedTextField() {
@@ -45,4 +72,144 @@ fun MyTitleOutlinedTextField() {
             keyboardType = KeyboardType.Text
         )
     )
+}
+
+//@Composable
+//fun CustomSearchField() {
+//    var text by remember { mutableStateOf(TextFieldValue("")) }
+//    val linearGradientBrush = myLinearGradiant()
+//
+//    Box(
+//        modifier = Modifier
+//            .padding(16.dp)
+//            .clip(RoundedCornerShape(8.dp))
+//            .background(Color.White)
+//            .height(50.dp)
+//            .fillMaxWidth(),
+//        contentAlignment = Alignment.CenterStart
+//    ) {
+//        Row(
+//            modifier = Modifier
+//                .fillMaxSize()
+//                .padding(end = 8.dp),
+//            verticalAlignment = Alignment.CenterVertically
+//        ) {
+//            Box(
+//                modifier = Modifier
+//                    .size(50.dp)
+//                    .clip(CircleShape)
+//                    .background(Color.LightGray.copy(alpha = 0.5f))
+//                    .padding(4.dp),
+//                contentAlignment = Alignment.Center
+//            ) {
+//                Icon(
+//                    imageVector = ImageVector.vectorResource(id = R.drawable.baseline_search_24), // Your pink search icon
+//                    contentDescription = "Search Icon",
+//                    modifier = Modifier
+//                        .size(32.dp)
+//                        .graphicsLayer(alpha = 0.99f)
+//                        .drawWithCache {
+//                            onDrawWithContent {
+//                                drawContent()
+//                                drawRect(linearGradientBrush, blendMode = BlendMode.SrcAtop)
+//                            }
+//                        },
+//                )
+//            }
+//            Spacer(modifier = Modifier.width(8.dp))
+//            Box(modifier = Modifier.weight(1f)) {
+//                if (text.text.isEmpty()) {
+//                    Text(
+//                        text = "Text Here....",
+//                        color = Color(0xFFD3A4D3),
+//                        fontSize = 16.sp,
+//                        modifier = Modifier.align(Alignment.CenterStart)
+//                    )
+//                }
+//                BasicTextField(
+//                    value = text,
+//                    onValueChange = { newText -> text = newText },
+//                    textStyle = LocalTextStyle.current.copy(
+//                        color = Color.Black,
+//                        fontSize = 16.sp,
+//                        textAlign = TextAlign.Start
+//                    ),
+//                    modifier = Modifier
+//                        .fillMaxWidth()
+//                )
+//            }
+//        }
+//    }
+//}
+
+@Composable
+fun CustomSearchField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    onSearch: () -> Unit
+) {
+    var text by remember { mutableStateOf(TextFieldValue(value)) }
+    val linearGradientBrush = myLinearGradiant()
+    Box(
+        modifier = Modifier
+            .clip(RoundedCornerShape(8.dp))
+            .background(Color.White)
+            .height(50.dp)
+            .fillMaxWidth(),
+        contentAlignment = Alignment.CenterStart
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(end = 8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Box(
+                modifier = Modifier
+                    .size(50.dp)
+                    .clip(CircleShape)
+                    .padding(4.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Icon(
+                    imageVector = ImageVector.vectorResource(id = R.drawable.baseline_search_24), // Your pink search icon
+                    contentDescription = "Search Icon",
+                    modifier = Modifier
+                        .size(32.dp)
+                        .graphicsLayer(alpha = 0.99f)
+                        .drawWithCache {
+                            onDrawWithContent {
+                                drawContent()
+                                drawRect(linearGradientBrush, blendMode = BlendMode.SrcAtop)
+                            }
+                        },
+                )
+            }
+            Spacer(modifier = Modifier.width(8.dp))
+            Box(modifier = Modifier.weight(1f)) {
+                if (text.text.isEmpty()) {
+                    Text(
+                        text = "Text Here....",
+                        color = Color(0xFFD3A4D3),
+                        fontSize = 18.sp,
+                        modifier = Modifier.align(Alignment.CenterStart)
+                    )
+                }
+                BasicTextField(
+                    value = text,
+                    onValueChange = { newText ->
+                        text = newText
+                        onValueChange(newText.text)
+                    },
+                    textStyle = LocalTextStyle.current.copy(
+                        color = Color.Black,
+                        fontSize = 16.sp,
+                        textAlign = TextAlign.Start
+                    ),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                )
+            }
+        }
+    }
 }
--- app/src/main/java/com/neatplex/nightell/ui/search/SearchScreen.kt
+++ app/src/main/java/com/neatplex/nightell/ui/search/SearchScreen.kt
@@ -2,12 +2,12 @@ package com.neatplex.nightell.ui.search
 
 import android.annotation.SuppressLint
 import android.net.Uri
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
@@ -17,16 +17,13 @@ import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -35,98 +32,77 @@ import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.neatplex.nightell.R
+import com.neatplex.nightell.component.CustomSearchField
 import com.neatplex.nightell.component.post.HomePostCard
 import com.neatplex.nightell.ui.viewmodel.SharedViewModel
 import com.neatplex.nightell.utils.toJson
 
 
-
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
-fun SearchScreen(navController: NavController, sharedViewModel: SharedViewModel, searchViewModel: SearchViewModel = hiltViewModel()) {
+fun SearchScreen(
+    navController: NavController,
+    sharedViewModel: SharedViewModel,
+    searchViewModel: SearchViewModel = hiltViewModel()
+) {
 
     var query by remember { mutableStateOf("") }
     val posts by searchViewModel.posts.observeAsState(emptyList())
     val isLoading by searchViewModel.isLoading.observeAsState(false)
     var lastPostId by remember { mutableStateOf<Int?>(null) }
 
-    //searchViewModel.search(query, null, false)
+    searchViewModel.search(query, null, false)
 
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = {
-                    OutlinedTextField(
-                        value = query,
-                        onValueChange = { newQuery ->
-                            query = newQuery
-                            searchViewModel.search(newQuery, null, false)
-                        },
-                        label = { Text("Search") },
-                        trailingIcon = {
-                            IconButton(onClick = { }) {
-                                Icon(
-                                    painter = painterResource(R.drawable.baseline_search_24),
-                                    contentDescription = null
-                                )
-                            }
-                        },
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(4.dp),
-                        keyboardOptions = KeyboardOptions(
-                            imeAction = ImeAction.Search
-                        ),
-                        keyboardActions = KeyboardActions(
-                            onSearch = {
+    Column(
+        modifier = Modifier
+            .background(color = Color.LightGray.copy(alpha = 0.5f))
+    ) {
 
-                            }
-                        )
-                    )
+        Row(
+            modifier = Modifier
+                .padding(16.dp)
+        ) {
+            CustomSearchField(
+                value = query,
+                onValueChange = { newQuery ->
+                    query = newQuery
+                    searchViewModel.search(newQuery, null, false)
                 },
-                backgroundColor = Color.White,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(80.dp),
-                elevation = 4.dp
-            )
-        },
-        content = { space ->
-            Box(modifier = Modifier.padding(space)) {
-                Column(
-                    modifier = Modifier
-                        .fillMaxSize()
-                ) {
-                    LazyColumn(
-                        contentPadding = PaddingValues(bottom = 65.dp),
-                        modifier = Modifier.fillMaxSize(),
-                        content = {
-                            itemsIndexed(posts!!) { index, post ->
-                                HomePostCard(post = post) { selectedPost ->
-                                    sharedViewModel.setPost(selectedPost)
-                                    val postJson = selectedPost.toJson()
-                                    navController.navigate(
-                                        "postScreen/${Uri.encode(postJson)}"
-                                    )
-                                }
-                                if (index == posts!!.size - 1 && !isLoading) {
-                                    lastPostId = post.id
-                                    searchViewModel.search(query, lastPostId , true)
-                                }
-                            }
-                            if (isLoading) {
-                                item {
-                                    CircularProgressIndicator(
-                                        modifier = Modifier
-                                            .padding(vertical = 16.dp)
-                                            .align(Alignment.CenterHorizontally)
-                                    )
-                                }
-                            }
-                        }
-                    )
+                onSearch = {
+                    searchViewModel.search(query, null, false)
                 }
-            }
+            )
         }
-    )
+
+        Row(Modifier.padding(vertical = 8.dp)) {
+            LazyColumn(
+                contentPadding = PaddingValues(bottom = 65.dp),
+                modifier = Modifier.fillMaxSize(),
+                content = {
+                    itemsIndexed(posts!!) { index, post ->
+                        HomePostCard(post = post) { selectedPost ->
+                            sharedViewModel.setPost(selectedPost)
+                            val postJson = selectedPost.toJson()
+                            navController.navigate(
+                                "postScreen/${Uri.encode(postJson)}"
+                            )
+                        }
+                        if (index == posts!!.size - 1 && !isLoading) {
+                            lastPostId = post.id
+                            searchViewModel.search(query, lastPostId, true)
+                        }
+                    }
+                    if (isLoading) {
+                        item {
+                            CircularProgressIndicator(
+                                modifier = Modifier
+                                    .padding(vertical = 16.dp)
+                            )
+                        }
+                    }
+                }
+            )
+        }
+
+    }
 }
