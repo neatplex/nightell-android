0be94c3b11bce0b0cdcf9decb4a78a9f64f21522
Anahita <anahita.tavakoli71@gmail.com>
2024-07-31 16:40:12 +0200

    handle requests when internet is weak

--- app/src/main/java/com/neatplex/nightell/data/network/RateLimiterInterceptor.kt
+++ app/src/main/java/com/neatplex/nightell/data/network/RateLimiterInterceptor.kt
@@ -1,12 +1,16 @@
 package com.neatplex.nightell.data.network
 
+import android.util.Log
 import okhttp3.Interceptor
 import okhttp3.Response
 import javax.inject.Inject
 
 class RateLimiterInterceptor @Inject constructor(private val rateLimiter: RateLimiter) : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
-        rateLimiter.acquire()
-        return chain.proceed(chain.request())
+        return rateLimiter.acquire()
+            .let {
+                Log.d("RateLimiterInterceptor", "Request acquired")
+                chain.proceed(chain.request())
+            }
     }
 }
--- app/src/main/java/com/neatplex/nightell/data/network/RetryInterceptor.kt
+++ app/src/main/java/com/neatplex/nightell/data/network/RetryInterceptor.kt
@@ -1,5 +1,8 @@
 package com.neatplex.nightell.data.network
 
+import android.util.Log
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
 import okhttp3.Interceptor
 import okhttp3.Response
 import java.io.IOException
@@ -14,14 +17,20 @@ class RetryInterceptor(private val maxRetries: Int) : Interceptor {
 
         while (tryCount < maxRetries && (response == null || !response.isSuccessful)) {
             try {
+                response?.close()
                 response = chain.proceed(request)
             } catch (e: IOException) {
                 exception = e
+                Log.e("RetryInterceptor", "IOException: retrying... ($tryCount/$maxRetries)", e)
             }
 
             tryCount++
             if (response == null || !response.isSuccessful) {
-                Thread.sleep((2.0.pow(tryCount) * 1000).toLong()) // Exponential backoff
+                val backoff = (2.0.pow(tryCount) * 1000).toLong().coerceAtMost(16000) // Cap backoff time
+                Log.d("RetryInterceptor", "Retry $tryCount, backoff $backoff ms")
+                runBlocking {
+                    delay(backoff)
+                }
             }
         }
 
