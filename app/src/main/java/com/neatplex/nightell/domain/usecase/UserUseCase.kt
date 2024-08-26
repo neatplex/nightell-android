78a89494fd5675b5e65ca6c285102725485bda32
Anahita <anahita.tavakoli71@gmail.com>
2024-08-26 15:29:52 +0200

    optimize usecase

--- app/src/main/java/com/neatplex/nightell/domain/usecase/UserUseCase.kt
+++ app/src/main/java/com/neatplex/nightell/domain/usecase/UserUseCase.kt
@@ -1,9 +1,5 @@
 package com.neatplex.nightell.domain.usecase
 
-import com.neatplex.nightell.data.dto.Profile
-import com.neatplex.nightell.data.dto.UserResponse
-import com.neatplex.nightell.data.dto.UserUpdated
-import com.neatplex.nightell.data.dto.Users
 import com.neatplex.nightell.domain.model.User
 import com.neatplex.nightell.domain.repository.IUserRepository
 import com.neatplex.nightell.utils.Result
@@ -11,29 +7,17 @@ import javax.inject.Inject
 
 class UserUseCase @Inject constructor(private val userRepository: IUserRepository) {
 
-    suspend fun profile(): Result<Profile> {
-        return userRepository.fetchProfile()
-    }
+    suspend fun profile() = userRepository.fetchProfile()
 
-    suspend fun getUserProfile(userId: Int): Result<UserResponse> {
-        return userRepository.showUserProfile(userId)
-    }
+    suspend fun getUserProfile(userId: Int) = userRepository.showUserProfile(userId)
 
-    suspend fun changeProfileName(name: String): Result<UserUpdated> {
-        return userRepository.changeProfileName(name)
-    }
+    suspend fun changeProfileName(name: String) = userRepository.changeProfileName(name)
 
-    suspend fun changeProfileBio(bio: String): Result<UserUpdated> {
-        return userRepository.changeProfileBio(bio)
-    }
+    suspend fun changeProfileBio(bio: String) = userRepository.changeProfileBio(bio)
 
-    suspend fun changeProfileUsername(username: String): Result<UserUpdated> {
-        return userRepository.changeProfileUsername(username)
-    }
+    suspend fun changeProfileUsername(username: String) = userRepository.changeProfileUsername(username)
 
-    suspend fun deleteAccount(): Result<Unit> {
-        return userRepository.deleteAccount()
-    }
+    suspend fun deleteAccount() = userRepository.deleteAccount()
 
     suspend fun searchUser(query: String, lastId: Int?): Result<List<User>?> {
         val result = userRepository.searchUser(query, lastId)
